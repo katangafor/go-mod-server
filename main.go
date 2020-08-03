f71c5bb5fb6a45b15da3281aee8c55c775bbf5f0
Johnny Hane <johnnykhane@gmail.com>
2020-08-03 17:11:40 -0400

    configuring heroku

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("alrighty")
+	// get the port
+	port, err := getPort()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// GET /
+	http.HandleFunc("/", hello)
+	// start the server
+	log.Printf("Listening on %s...\n", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		panic(err)
+	}
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World!")
+}
+func getPort() (string, error) {
+	// the PORT is supplied by Heroku
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
 }
